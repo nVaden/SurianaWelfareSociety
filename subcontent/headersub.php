7785ce6038c699b381b40d7ce3a5cc5c67a9b24c
RoyaK <roya.k440@gmail.com>
2017-09-15 14:55:17 +0800

    footer and headers

---
+++ subcontent/headersub.php
@@ -0,0 +1,17 @@
+<?php
+include_once '../vals/inc/configAll.php';
+
+echo '<script src="'.$defaulturl['home'].'/assets/js/pins/jquery.js"></script>
+	  <script src="'.$defaulturl['home'].'/assets/js/pins/bootstrap.min.js"></script>
+	  <script src="'.$defaulturl['home'].'/assets/js/pins/jquery-confirm.min.js"></script>
+	  <script src="'.$defaulturl['home'].'/assets/js/main.js"></script>';
+
+echo '<link href="'.$defaulturl['home'].'/assets/css/pins/bootstrap.min.css" rel="stylesheet">
+	  <link href="'.$defaulturl['home'].'/assets/css/pins/bootstrap.css" rel="stylesheet">
+	  <link href="'.$defaulturl['home'].'/assets/css/pins/jquery-confirm.min.css" rel="stylesheet">
+	  <link href="'.$defaulturl['home'].'/assets/css/pins/w3-theme-black.css" rel="stylesheet">
+	  <link href="'.$defaulturl['home'].'/assets/css/pins/w3.css" rel="stylesheet">
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">';
+
+
+?>
