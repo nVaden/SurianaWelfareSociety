9e1e63353bacf8827145ac49a1bfab45af9a804d
Nicholas Vaden <vaden.nicholas@gmail.com>
2017-09-18 12:18:47 +0800

    pages added, layout process

--- subcontent/footer.php
+++ subcontent/footer.php
@@ -4,6 +4,9 @@ echo '<script src="'.$defaulturl['home'].'/assets/js/pins/jquery.js"></script>';
 echo '<script src="'.$defaulturl['home'].'/assets/js/pins/bootstrap.min.js"></script>';
 echo '<script src="'.$defaulturl['home'].'/assets/js/pins/jquery-confirm.min.js"></script>';
 echo '<script src="'.$defaulturl['home'].'/assets/js/main.js"></script>';
-
+echo '<script type="text/javascript" src="assets/js/jquery.js"></script>';
+echo '<script type="text/javascript" src="assets/js/jquery-migrate.min.js"></script>';
+echo '<script type="text/javascript" src="assets/slick/slick.js"></script>';
+echo '<script type="text/javascript" src="assets/js/main.js"></script>';
 
 ?>
