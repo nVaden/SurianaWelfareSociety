198ef145d4ea265c9293d85650250743fe668ab4
Nicholas Vaden <vaden.nicholas@gmail.com>
2017-09-18 15:22:07 +0800

    input fields placed volunteer.php and index.php modals

---
+++ subcontent/nav.php
@@ -0,0 +1,32 @@
+
+
+<div class="w3-top">
+ <div class="w3-bar w3-theme-d2 w3-left-align">
+  <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-hover-white w3-theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
+  <a href="#" class="w3-bar-item w3-button w3-teal"><i class="fa fa-home w3-margin-right"></i>Suriana Welfare Society Of Malaysia</a>
+  <a href="#test-target" class="w3-bar-item w3-button w3-hide-small w3-hover-white">What We Do</a>
+  <a href="/Suriana/blog.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Blog</a>
+  <a href="/Suriana/volunteer.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Take Action</a>
+  <a href="/Suriana/contact.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Contact</a>
+
+  <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-hover-teal" title="Search"><i class="fa fa-search"></i></a>
+<!--     <div class="w3-dropdown-hover w3-hide-small">
+      <button class="w3-button" title="Notifications">Take Action <i class="fa fa-caret-down"></i></button>     
+      <div class="w3-dropdown-content w3-card-4 w3-bar-block">
+        <a href="#" class="w3-bar-item w3-button">Link</a>
+        <a href="#" class="w3-bar-item w3-button">Link</a>
+        <a href="#" class="w3-bar-item w3-button">Link</a>
+      </div>
+    </div> -->
+
+  </div>
+
+  <!-- Navbar on small screens -->
+  <div id="navDemo" class="w3-bar-block w3-theme-d2 w3-hide w3-hide-large w3-hide-medium">
+    <a href="#team" class="w3-bar-item w3-button">Team</a>
+    <a href="#work" class="w3-bar-item w3-button">Work</a>
+    <a href="#pricing" class="w3-bar-item w3-button">Price</a>
+    <a href="#contact" class="w3-bar-item w3-button">Contact</a>
+    <a href="#" class="w3-bar-item w3-button">Search</a>
+  </div>
+</div>
